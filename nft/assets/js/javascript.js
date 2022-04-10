ec625bc540efc5fab301275fbf182831d3d8ed69
eun98bi <103350895+eun98bi@users.noreply.github.com>
2022-04-10 20:16:19 +0900

    Add files via upload

---
+++ nft/assets/js/javascript.js
@@ -0,0 +1,19 @@
+const categorys = document.querySelectorAll(".category");
+const titles = document.querySelectorAll(".title");
+const one = titles[0].offsetTop-200;
+const two = titles[1].offsetTop-150;
+const three = titles[2].offsetTop-150;
+
+categorys[0].onclick = function(){
+  window.scroll({top:one, behavior: 'smooth'})
+}
+categorys[1].onclick = function(){
+  window.scroll({top:two, behavior: 'smooth'})
+}
+categorys[2].onclick = function(){
+  window.scroll({top:three, behavior: 'smooth'})
+}
+
+
+
+
